bf286800804cf3847f8322b43d2220eec47289d5
GEOkettle <jsonmiou@gmail.com>
2022-06-29 00:02:47 +0900

    new start

---
+++ codingdevil/01_basictype.ts
@@ -0,0 +1,24 @@
+"use strict";
+
+
+//string
+let car: string = "hummer";
+car = 'rubicon'
+// 선언한 타입이 아닌 다른타입을 넣으면 error
+// car = 12
+
+
+//number
+let age: number = 29
+//boolean
+let isadult: boolean = true
+//array
+let a: number[] = [1, 2, 3]
+let a2: Array<number> = [1, 2, 3]
+
+let week: string[] = ['sun', 'mon', 'tues']
+let week2: Array<string> = ['sun', 'mon', 'tues',]
+
+//tuple
+
+//https://medium.com/the-clever-dev/the-difference-between-a-tuple-and-an-array-in-typescript-e9fe9cc4a862
